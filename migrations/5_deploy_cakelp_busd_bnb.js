43ab496e2a11f61f1fb5aafe2d290cc76e8798e3
Dev <dev@lumaris.finance>
2021-01-26 11:59:55 -0600

    Added lp busd-bnb token

---
+++ migrations/5_deploy_cakelp_busd_bnb.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_BUSD_BNB.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0x1B96B92314C44b159149f7E0303511fB2Fc4774f", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        2
+    )
+}
