1ae207f4459dc96692c496ea55bc49642c2e159e
Dev <dev@lumaris.finance>
2021-01-26 11:58:31 -0600

    Added lp cake-bnb token

---
+++ migrations/3_deploy_cakelp_cake_bnb.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_CakeBNB.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        1
+    )
+}
