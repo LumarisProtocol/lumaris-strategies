7f1bf237bdf3aa85245ee40226ad5b2986a4f217
Dev <dev@lumaris.finance>
2021-01-26 12:10:28 -0600

    Added lp usdc-busd token

---
+++ migrations/7_deploy_cakelp_usdc_busd.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_USDC_BUSD.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0x680Dd100E4b394Bda26A59dD5c119A391e747d18", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        53
+    )
+}
