b0acf59519df1755e79df872b6e59242d2f4261e
Dev <dev@lumaris.finance>
2021-01-26 12:12:07 -0600

    Added lp vai-busd token

---
+++ migrations/11_deploy_cakelp_vai_busd.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_VAI_BUSD.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0xfF17ff314925Dff772b71AbdFF2782bC913B3575", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        41
+    )
+}
