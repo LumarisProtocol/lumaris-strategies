dd8b308caba573afb7b48d217e589116c2c1b5d2
Dev <dev@lumaris.finance>
2021-01-26 12:10:51 -0600

    Added lp bscx-bnb token

---
+++ migrations/8_deploy_cakelp_bscx_bnb.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_BSCX_BNB.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0x20781bc3701C5309ac75291f5D09BdC23D7b7Fa8", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        51
+    )
+}
