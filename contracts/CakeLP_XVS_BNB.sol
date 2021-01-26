73944969717100693b22b8334d135908ad21c670
Dev <dev@lumaris.finance>
2021-01-26 12:23:51 -0600

    Added lp xvs_bnb token

---
+++ contracts/CakeLP_XVS_BNB.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./CakeLP.sol";
+
+contract CakeLP_XVS_BNB is CakeLP {
+    using SafeBEP20 for IBEP20;
+    
+    constructor(string memory _name, string memory _alias, IBEP20 _tokenMain, IBEP20 _token, IController _controller, IMasterChef _masterchef, ICakePool _cakePool, uint256 _pid) 
+    CakeLP(_name, _alias, _tokenMain, _token, _controller, _masterchef, _cakePool, _pid)
+    public {}
+
+}
---
+++ migrations/15_deploy_cakelp_xvs_bnb.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_XVS_BNB.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0x41182c32F854dd97bA0e0B1816022e0aCB2fc0bb", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        13
+    )
+}
