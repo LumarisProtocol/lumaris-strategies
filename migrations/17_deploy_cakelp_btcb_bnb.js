2faab055456a6e76858121adabbe51557efcf202
Dev <dev@lumaris.finance>
2021-01-26 12:24:27 -0600

    Added lp btcb_bnb token

---
+++ migrations/17_deploy_cakelp_btcb_bnb.js
@@ -0,0 +1,15 @@
+const LP = artifacts.require('./CakeLP_BTCB_BNB.sol')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        LP,
+        process.env.NAME,
+        process.env.ALIAS,
+        process.env.TOKEN_MAIN,
+        "0x7561EEe90e24F3b348E1087A005F78B4c8453524", //tokenLP
+        process.env.CONTROLLER,
+        process.env.MASTERCHEF,
+        process.env.CAKE_POOL,
+        15
+    )
+}
